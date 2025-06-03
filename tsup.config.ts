304cee8b4fc84e4aa2734e806e72a8ee46654b7d
Omar Mir <omarmir@gmail.com>
2025-06-02 23:53:55 -0400

    fix: export seperately

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
+  entry: ['src/index.ts', 'src/regex.ts'],
+  format: ['esm'],
   dts: true, // This enables declaration file generation
   splitting: false,
   sourcemap: true,
   clean: true,
   minify: true,
   external: [
-    "@milkdown/core",
-    "@milkdown/prose",
-    "@milkdown/ctx",
-    "@milkdown/transformer",
-    "@milkdown/utils",
-    "@milkdown/kit",
-    "@milkdown/preset-commonmark",
-    "atomico",
-    "clsx",
-    "material-file-icons",
-    "remark-directive",
+    '@milkdown/core',
+    '@milkdown/prose',
+    '@milkdown/ctx',
+    '@milkdown/transformer',
+    '@milkdown/utils',
+    '@milkdown/kit',
+    '@milkdown/preset-commonmark',
+    'atomico',
+    'clsx',
+    'material-file-icons',
+    'remark-directive'
   ],
-  treeshake: true,
+  treeshake: true
 })
